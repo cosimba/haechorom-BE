1007e0a2957d27775e3eb24b547403b1512919bc
sso9594 <sso9594@naver.com>
2024-09-26 00:36:29 +0900

    chore: elasticsearch 미사용에 따른 코드 수정

--- src/dive/src/main/java/com/cosimba/dive/domain/clean/entity/Clean.java
+++ src/dive/src/main/java/com/cosimba/dive/domain/clean/entity/Clean.java
@@ -1,53 +1,88 @@
 package com.cosimba.dive.domain.clean.entity;
 
+import com.cosimba.dive.global.entity.BaseEntity;
 import com.cosimba.dive.global.entity.TrashType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
-@Document(indexName = "josa_trash")
-public class Clean {
-    @Id
-    private String id;
+@Entity
+public class Clean extends BaseEntity {
 
-    @Field(name = "clean_name")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "clean_name")
     private String cleanName;
 
-    @Field(name = "serial_number")
+    @Column(name = "serial_number")
     private String serialNumber;
 
-    @Field(name = "clean_date")
+    @Column(name = "clean_date")
     private LocalDateTime cleanDate;
 
-    @Field(name = "coast_name")
+    @Column(name = "coast_name")
     private String coastName;
 
     private double lat;
 
     private double lng;
 
-    @Field(name = "coast_length")
+    @Column(name = "coast_length")
     private Integer coastLength;
 
-    @Field(name = "collect_bag")
+    @Column(name = "collect_bag")
     private Integer collectBag;
 
-    @Field(name = "collect_val")
+    @Column(name = "collect_val")
     private Integer collectVal;
 
-    @Field(name = "trash_type")
+    @Column(name = "trash_type")
     private TrashType trashType;
 
-    @Field(name = "clean_status")
+    @Column(name = "clean_status")
     private CleanStatus cleanStatus;
+
+    public static Clean create(String cleanName, String serialNumber, LocalDateTime cleanDate,
+                               String coastName, double lat, double lng, Integer coastLength,
+                               Integer collectBag, Integer collectVal, TrashType trashType,
+                               CleanStatus cleanStatus) {
+        return Clean.builder()
+                .cleanName(cleanName)
+                .serialNumber(serialNumber)
+                .cleanDate(cleanDate)
+                .coastName(coastName)
+                .lat(lat)
+                .lng(lng)
+                .coastLength(coastLength)
+                .collectBag(collectBag)
+                .collectVal(collectVal)
+                .trashType(trashType)
+                .cleanStatus(cleanStatus)
+                .build();
+    }
+
+    public void update(String cleanName, String serialNumber, LocalDateTime cleanDate, String coastName, double lat, double lng, Integer coastLength, Integer collectBag, Integer collectVal, TrashType trashType, CleanStatus cleanStatus) {
+        this.cleanName = cleanName;
+        this.serialNumber = serialNumber;
+        this.cleanDate = cleanDate;
+        this.coastName = coastName;
+        this.lat = lat;
+        this.lng = lng;
+        this.coastLength = coastLength;
+        this.collectBag = collectBag;
+        this.collectVal = collectVal;
+        this.trashType = trashType;
+        this.cleanStatus = cleanStatus;
+    }
 }
