bab182033bcde546c88049dc5164aeca3b11a91f
sso9594 <sso9594@naver.com>
2024-09-25 18:46:40 +0900

    feat: 쓰레기 타입 enum

---
+++ src/dive/src/main/java/com/cosimba/dive/global/entity/TrashType.java
@@ -0,0 +1,15 @@
+package com.cosimba.dive.global.entity;
+
+public enum TrashType {
+    FISHING_NET("폐어구류"),
+    BUOY("부표류"),
+    DAILY_TRASH("생활쓰레기류"),
+    LARGE_TRASH("대형 투기쓰레기류"),
+    WOOD("초목류");
+
+    private final String trashType;
+
+    TrashType(String trashType) {
+        this.trashType = trashType;
+    }
+}
